fbbfed346af48208ac3e262cfbb362dbe55b15fc
EHB-4 <rafi.cdm5@gmail.com>
2025-06-28 00:19:29 +0500

    EHB Auto-Push to Cursor: 2025-06-27T19:19:29.061Z - .git\logs\HEAD, .git\logs\refs\heads\main, .git\logs\refs\remotes\origin\main, .git\index, .git\COMMIT_EDITMSG

---
+++ app/package-test/page.tsx
@@ -0,0 +1,9 @@
+import { PackageTest } from '../../components/PackageTest';
+
+export default function PackageTestPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PackageTest />
+    </div>
+  );
+}
